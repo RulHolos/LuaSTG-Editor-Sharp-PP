f7936044ca4de1252865fac3500059d3edb6f30a
Guy-L <g.guy.lab@gmail.com>
2026-07-12 22:34:20 +0200

    add fallback to _editor_class collection when transpiling base class selection

--- LuaSTGNode.Legacy/EditorData/Node/Object/ObjectDefine.cs
+++ LuaSTGNode.Legacy/EditorData/Node/Object/ObjectDefine.cs
@@ -68,7 +68,7 @@ namespace LuaSTGEditorSharp.EditorData.Node.Object
             string difficultyS = NonMacrolize(1) == "All" ? "" : ":" + NonMacrolize(1);
 
             string typeInput = NonMacrolize(2);
-            string typeStr = string.IsNullOrEmpty(typeInput) ? "_object" : typeInput;
+            string typeStr = string.IsNullOrEmpty(typeInput) ? "_object" : (typeInput + " or _editor_class[\"" + typeInput + "\"]");
 
             yield return sp + "_editor_class[\"" + Lua.StringParser.ParseLua(NonMacrolize(0) + difficultyS) + "\"] = Class(" + typeStr + ")\n";
             foreach (var a in base.ToLua(spacing))
